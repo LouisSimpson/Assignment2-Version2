27f27271d0d2a4320ef03b4f93a1d8388d6776b1
ReubenPorter <aboutbillionsab@gmail.com>
2017-12-11 15:58:41 +0000

    made more improvements on the guardian api and added styling

--- js/guardian.js
+++ js/guardian.js
@@ -4,10 +4,10 @@
 
   let queryBox = document.getElementById("guardQuery");
   let searchForm = document.getElementById("searchForm");
-  let demoJSON = document.getElementById("demo");
+  let demoJSON = document.getElementById("results");
 
   // get an api key from http://open-platform.theguardian.com/access/
-  let baseURL = "https://content.guardianapis.com/search?api-key=317c6d50-efc8-4a5d-a28d-ce37f5f37e87&q=";
+  let baseURL = "https://content.guardianapis.com/search?api-key=317c6d50-efc8-4a5d-a28d-ce37f5f37e87&q=motogp&show-fields=thumbnail";
 
   searchForm.addEventListener("submit", function(ev){
     let url = baseURL + queryBox.value;
@@ -23,7 +23,7 @@
         let theData = "";
         let tmp = data.response.results;
         for (key in tmp) {
-          theData += `<li><a href="${tmp[key].webUrl}">${tmp[key].webTitle}</a> ${tmp[key].webPublicationDate}</li>`;
+          theData += `<li><a href="${tmp[key].webUrl}"><h3>${tmp[key].webTitle}</h3></a> ${tmp[key].webPublicationDate} ${tmp[key].thumbnail}</li>`;
         }
         demoJSON.innerHTML = theData;
       });
